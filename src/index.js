31182270e2e14ff359ab236927668e8f8708133d
Kevin <kevinkuchar2378@gmail.com>
2016-08-25 01:22:55 -0400

    section 17

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBJtP0huRFyeAuBjfWlzTIg6DMDXNKqzYs';
+
 // Create a new component
 const App = () => {
-    return <div>Hi!</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // Put component in DOM
